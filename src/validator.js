69b8bba25a1c577b07f9957b329495cbd5c6b726
gilliam24 <gilliamargelmonterroza@gmail.com>
2022-01-18 16:09:33 -0500

    funcion 1

--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,16 @@
-//const validator = {
-  // ...
-//
-  // Obtengo el nombre del input
-  
+function validator(){
+let numpair=0;
+let numodd=0;
+let numTcString = numbertc.toString().split("");
+for (let i = 0; i< numbertcToString.length; i++){
+
+  if(i % 2 === 0){   //verificamos si la posicion es par 
+    if(numTcString[i] * 2 >= 10){
+      numpair += ((numto))
+    }
+  }
+}
+
+}
 
 
-
-//export default validator;
