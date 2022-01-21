6e8ee8fccb5084b2229b3edd3e16eec916041df5
gilliam24 <gilliamargelmonterroza@gmail.com>
2022-01-21 13:04:26 -0500

    algoritmo de luhn, maskify, isvalid

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-//import validator from './validator.js';
-let btnntc=document.getElementById("btn-ntc");
-btnntc.addEventListener('click',()=>{
+import validator from './validator.js';
+function comprar() {
+  document.getElementById("pag1").style.display = 'none';
+  document.getElementById("pag2").style.display = 'block';
+}
+const comprar1 = document.getElementById("btnimg1");
+const comprar2 = document.getElementById("btnimg2");
+const comprar3 = document.getElementById("btnimg3");
+comprar1.addEventListener("click", () => comprar1())
+comprar2.addEventListener("click", () => comprar2())
+comprar3.addEventListener("click", () => comprar3())
 
-    let numbertc= document.getElementById("numbertc").value;
-    return console.log(numbertc);
+
+let btnntc = document.getElementById("btn-ntc");
+btnntc.addEventListener('click', () => {
+  let numbertc = document.getElementById("cardNumber").value;
+  validator.isValid(numbertc)
+  validator.maskify(numbertc)
+
+  
+  
+  return console.log(numbertc);
 })
-
 
--- src/validator.js
+++ src/validator.js
@@ -1,16 +1,36 @@
-function validator(){
-let numpair=0;
-let numodd=0;
-let numTcString = numbertc.toString().split("");
-for (let i = 0; i< numbertcToString.length; i++){
+const validator = {
+  isValid: (cardNumber) => {
+    let numPair = 0;
+    let numOdd = 0;
+    cardNumber = cardNumber.split("").reverse();
+    for (let i = 0; i < cardNumber.length; i++) {
+      if (i % 2 === 1) {        //verificamos si la posicion es par 
+        if (cardNumber[i] * 2 >= 10) {
+          numPair += ((cardNumber[i] * 2) - 9);
+        } else {
+          numPair += cardNumber[i] * 2;
+        }
 
-  if(i % 2 === 0){   //verificamos si la posicion es par 
-    if(numTcString[i] * 2 >= 10){
-      numpair += ((numto))
+      } else {
+        numOdd += parseInt(cardNumber[i]);
+      }
+
     }
-  }
-}
+    //return console.log(numPair + numOdd) % 10 === 0;
 
+  },
+
+
+maskify: (cardNumber) => {
+    if (cardNumber.length > 4) {
+      let result = cardNumber.lengt - 4;
+      console.log("#".repeat(result) + cardNumber.substr(-4));
+      return "#".repeat(result) + cardNumber.substr(-4);
+
+    }
+  },
+  
 }
+export default validator;
 
 
