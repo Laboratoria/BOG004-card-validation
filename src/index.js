72db55f9b0585e92287b042cc38cf52f56a0c7f9
zulaymejia <zulaymejia06@gmail.com>
2022-01-31 12:00:27 -0500

    funcionalidad correcta

--- src/index.js
+++ src/index.js
@@ -38,52 +38,5 @@ let numerosTarjeta = document.getElementById("numerosIngresados").value;
       validator.maskify(numerosTarjeta);
   }
 }
-// let sumaTotal = []
-// let sumaDefinitiva = sumaTotal.push(revertirNum);
-// console.log(sumaTotal)
-
-// //const sumaFinal = sumaDefinitiva;
-// const reducer = revertirNum.reduce( (accumulator, curr) => accumulator + +curr, 0);//no sabemos cómo hacer que sume, con - da el resultado en negativo.
-// console.log(reducer);
-// console.log(reducer % 10);
 );
- valReducer= reducer;
-
-//nos arroja alert si es válida o inválida, pero no nos imprime en la consola si es boolean o number.
-
-if(valReducer % 10 == 0) {
-  alert("Tarjeta válida")}
-else {
-  alert("Tarjeta inválida")
-//console.log(valReducer);
-}
-
-//Función para maskify
-/*function cardHide(numeros) {
-  let numerosOriginales = numeros.split("");
-  console.log(numerosOriginales);
-  let hideNum = [];
-	for(let i = 0; i < numeros.length; i++){
-    if(i < numeros.length-4){
-      hideNum.push("*");
-    }else{
-      hideNum.push(hideNum[i]);
-    }
-  }
-  let numeroOculto = hideNum.join("");
-  console.log(numeroOculto);
-  return numeroOculto;
-}*/
-const maskify = (numeros) => {
-  return numeros.slice(0, -4).replace(/[0-9]/g, '#').concat(numeros.slice(-4, numeros.len));
-}
-console.log(maskify(numeros)) //desired output should be: ##### #orld
-
  
-
-
-  const botonValidar=document.getElementById("btnValidar");
-  botonValidar.addEventListener("click",obtenerNumero);
-
-const maskifyFinal=document.getElementById("numerosIngresados");
-  maskifyFinal.addEventListener("write",obtenerNumero);
